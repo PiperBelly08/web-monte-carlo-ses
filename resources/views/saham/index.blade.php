904ae26300f19b95089f15251ac3fc8f41e20cfc
PiperBelly08 <hawarimunte@gmail.com>
2025-05-31 03:12:16 +0700

    style: update footer copyright year and enhance button layouts in Saham index

--- resources/views/saham/index.blade.php
+++ resources/views/saham/index.blade.php
@@ -1,27 +1,31 @@
 @extends('layouts.app')
-  
+
 @section('title', 'Saham')
-  
+
 @section('contents')
     <div style="d-flex align-items-center justify-content-between; gap: 10px;">
-        {{-- <h1 class="mb-0">Saham</h1> --}}
-        <a href="{{ route('saham.create') }}" class="btn btn-primary">Add Product</a>
-        <a href="{{ route('saham.export.pdf') }}" class="btn btn-outline-danger" target="_blank">Export PDF</a>
-       <form action="{{ route('saham.clear') }}" method="POST" enctype="multipart/form-data" onsubmit="return confirm('Hapus Semua Data?')" class="d-inline">
-            @csrf
-            <button type="submit" class="btn btn-outline-danger">Clear</button>
-        </form>
+        <div class="btn-group btn-group-sm" role="group" aria-label="Actions">
+            <a href="{{ route('saham.create') }}" class="btn btn-primary">Add Product</a>
+            <a href="{{ route('saham.export.pdf') }}" class="btn btn-outline-danger" target="_blank">Export PDF</a>
+            <form action="{{ route('saham.clear') }}" method="POST" enctype="multipart/form-data" onsubmit="return confirm('Hapus Semua Data?')" class="m-0 p-0">
+                @csrf
+                <button type="submit" class="btn btn-sm btn-outline-danger">Clear</button>
+            </form>
+        </div>
     </div>
     <div style="d-flex align-items-center justify-content-between; gap: 10px;">
         {{-- Form Upload --}}
         <form action="{{ route('saham.import') }}" method="POST" enctype="multipart/form-data">
             @csrf
-            <div class="mb-3">
-                <label for="file" class="form-label">Pilih File Excel atau CSV</label>
-                <input type="file" name="file" id="file" class="form-control" accept=".xlsx,.xls,.csv" required>
+            <div class="input-group my-3">
+                <div class="input-group-prepend">
+                    <button class="btn btn-outline-primary" type="submit">Import</button>
+                </div>
+                <div class="custom-file">
+                    <input type="file" name="file" id="file" class="custom-file-input" accept=".xlsx,.xls,.csv" required>
+                    <label for="file" class="custom-file-label" id="file">Pilih File Excel atau CSV</label>
+                </div>
             </div>
-            <button type="submit" class="btn btn-success">Import</button>
-            <a href="{{ route('saham.index') }}" class="btn btn-secondary">Kembali</a>
         </form>
     </div>
     <hr />
@@ -54,17 +58,17 @@
                             <td class="align-middle">{{ $rs->nama_saham }}</td>
                             <td class="align-middle">{{ $rs->open }}</td>
                             <td class="align-middle">{{ $rs->high }}</td>
-                            <td class="align-middle">{{ $rs->low }}</td> 
-                            <td class="align-middle">{{ $rs->close}}</td> 
+                            <td class="align-middle">{{ $rs->low }}</td>
+                            <td class="align-middle">{{ $rs->close}}</td>
                             <td class="align-middle">{{ $rs->volume }}</td>
                             <td class="align-middle">
-                                <div class="btn-group" role="group" aria-label="Basic example">
+                                <div class="btn-group btn-group-sm" role="group" aria-label="Actions">
                                     <a href="{{ route('saham.show', $rs->id) }}" type="button" class="btn btn-secondary">Detail</a>
                                     <a href="{{ route('saham.edit', $rs->id)}}" type="button" class="btn btn-warning">Edit</a>
-                                    <form action="{{ route('saham.destroy', $rs->id) }}" method="POST" type="button" class="btn btn-danger p-0" onsubmit="return confirm('Delete?')">
+                                    <form action="{{ route('saham.destroy', $rs->id) }}" method="POST" type="button" class="p-0 m-0" onsubmit="return confirm('Delete?')">
                                         @csrf
                                         @method('DELETE')
-                                        <button class="btn btn-danger m-0">Delete</button>
+                                        <button class="btn btn-sm btn-danger m-0">Delete</button>
                                     </form>
                                 </div>
                             </td>
