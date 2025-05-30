468ad94b00877104d9359b7ec4f5f1527850f317
PiperBelly08 <hawarimunte@gmail.com>
2025-05-31 02:38:41 +0700

    style: update sidebar branding and layout; change icon and text

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,23 +1,23 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-  
+
   <!-- Sidebar - Brand -->
   <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
     <div class="sidebar-brand-icon rotate-n-15">
-      <i class="fas fa-laugh-wink"></i>
+      <i class="fas fa-superscript"></i>
     </div>
-    <div class="sidebar-brand-text mx-3">Metode Monte Carlo <sup>2</sup></div>
+    <div class="sidebar-brand-text mx-3">Montesse</div>
   </a>
-  
+
   <!-- Divider -->
   <hr class="sidebar-divider my-0">
-  
+
   <!-- Nav Item - Dashboard -->
   <li class="nav-item">
     <a class="nav-link" href="{{ route('dashboard') }}">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Dashboard</span></a>
   </li>
-  
+
   <li class="nav-item">
     <a class="nav-link" href="{{ route('saham.index') }}">
       <i class="fas fa-fw fa-tachometer-alt"></i>
@@ -29,20 +29,20 @@
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Monte Carlo</span></a>
   </li>
-  
+
   <li class="nav-item">
     <a class="nav-link" href="/profile">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Profile</span></a>
   </li>
-  
+
   <!-- Divider -->
   <hr class="sidebar-divider d-none d-md-block">
-  
+
   <!-- Sidebar Toggler (Sidebar) -->
   <div class="text-center d-none d-md-inline">
     <button class="rounded-circle border-0" id="sidebarToggle"></button>
   </div>
-  
-  
+
+
 </ul>
